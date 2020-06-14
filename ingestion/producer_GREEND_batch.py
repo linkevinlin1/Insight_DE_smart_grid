ab492a19f46165f1c4b0eec1c47ca68de2020a76
Kevin Lin <linkevinlin1@gmail.com>
2020-06-14 01:47:50 -0500

    read from config files, add starting offset, loop reading, random factor

--- ingestion/producer_GREEND_batch.py
+++ ingestion/producer_GREEND_batch.py
@@ -5,47 +5,57 @@ import json
 import csv
 import os
 import glob
+import configparser
+import sys
+import json
+import random
+import copy
 
-datapath = '../../data'
-servers = 'localhost:9092'
-topic = "powerraw"
-# period for the data points
-playbackspeed = 24
+def load_config(file):
+    """
+    Load config file
+    """
+    config = configparser.ConfigParser()
+    config.read(file)
 
-labels = {
-    'building0': ['coffee machine', 'washing machine', 'radio', 'water kettle', 'fride w/ freezer', 'dishwasher', 'ktichen lamp', 'TV', 'vacuum cleaner'],
-    'building1': ['radio', 'freezer', 'dishwasher', 'fridge', 'washing machine', 'water kettle', 'blender', 'network router','unknown'],
-    'building2': ['fridge', 'dishwasher', 'microwave', 'water kettle', 'washing machine', 'radio w/ amplifier', 'dryier', 'kitchenware (mixer and fruit juicer)', 'bedside light'],
-    'building3': ['TV', 'NAS', 'washing machine', 'drier', 'dishwasher', 'notebook', 'kitchenware', 'coffee machine', 'bread machine'],
-    'building4': ['entrance outlet', 'dishwasher', 'water kettle', 'fridge w/o freezer', 'washing machine', 'hairdrier', 'computer', 'coffee machine', 'TV'],
-    'building5': ['total outlets', 'total lights', 'kitchen TV', 'living room TV', 'fridge w/ freezer', 'electric oven', 'computer w/ scanner and printer', 'washing machine', 'hood', 'unknown'],
-    'building6': ['plasma TV', 'lamp', 'toaster', 'hob', 'iron', 'computer w/ scanner and printer', 'LCD TV', 'washing machine', 'fridge w/ freezer'],
-    'building7': ['hair dryer', 'washing machine', 'videogame console and radio', 'dryer', 'TV w/ decoder and computer in living room', 'kitchen TV', 'dishwasher', 'total outlets', 'total lights'],
-    'building8': ['kitchen TV', 'dishwasher', 'living room TV', 'desktop computer w/ screen', 'washing machine', 'bedroom TV', 'total outlets', 'total lights'],
-    }
+    datapath = config['Data']['greend_path']
+    brokers = config['KafkaBrokers']['address']
+    topic = config['KafkaBrokers']['topic_rawdata']
+    # how many time faster playback
+    playbackspeed = int(config['Data']['playback_speed'])
+    return datapath, brokers, topic, playbackspeed
 
-def house_reader_GREEND(datapath):
+
+
+def house_reader_GREEND(path):
     '''
     Load the GREEND files into iterators.
-    @type datapath: STRING
-    @param datapath: the path to the data folder
+    @type path: STRING
+    @param path: the path to the data folder
 
     @rtype house_list: list[STRING]
     @return: list of house names
 
-    @rtype filenames: list[iterator]
-    @return: file name iterator for each house
+    @rtype filenames: list[STRING]
+    @return: file name list for each house
+
+    @rtype filenames: list[STRING]
+    @return: list of appliance names for each house
     '''
-    path = datapath+'/GREEND'
     house_list = sorted(os.listdir(path))
     house_list = [house for house in house_list if "building" in house]
     #print(sorted(house_list))
     filenames = [[] for _ in range(len(house_list))]
     for hid, house in enumerate(house_list):
         hpath = path + '/' + house
-        filenames[hid]=iter(sorted([os.path.basename(x) for x in glob.glob(hpath+"/dataset_201*.csv")]))
+        filenames[hid]=sorted([os.path.basename(x) for x in glob.glob(hpath+"/dataset_201*.csv")])
+
     #print(filenames[0])
-    return house_list, filenames
+    # load appliance labels
+    labels= {}
+    with open(path + '/labels.json') as json_file:
+        labels = json.load(json_file)
+    return house_list, filenames, labels
 
 
 def open_files(house_name, filename):
@@ -65,7 +75,7 @@ def open_files(house_name, filename):
     @return csviter: csv iterator
     '''
 
-    fd = open(datapath+'/GREEND/'+house_name + '/' + filename, newline='')
+    fd = open(datapath+ '/' +house_name + '/' + filename, newline='')
     #print(filename)
     csviter = csv.reader(fd, delimiter=',')
     next(csviter, None)
@@ -108,62 +118,71 @@ def is_number(s):
     except ValueError:
         return False
 
-i=0
-starttime=time.time()
-house_list, filenames = house_reader_GREEND(datapath)
 
-first_filenames = filenames
-# file descriptors for each house
-fileds = [None]*len(house_list)
-# offset for the current time to the timestamp in the files
-offsets = [None]*len(house_list)
-# first timestamp for the current file
-firsttimes = [None]*len(house_list)
-# previous timestamp for each house
-pretimes = [None]*len(house_list)
-# previous power values for each house
-prevalues = [None]*len(house_list)
-# csv iterator for row reading
-csviters = [None]*len(house_list)
+if __name__ == "__main__":
+    _, config, filestart_offset, batch_num = sys.argv
+    datapath, brokers, topic, playbackspeed = load_config(config)
+    starttime=time.time()
+    # introduce randomness to the power output
+    factor = random.uniform(0.8, 1.2)
+    house_list, filenames, labels = house_reader_GREEND(datapath)
 
-producer = kafka_init(servers, topic)
-
-while True:
-    print(i)
-    for house_id, house_name in enumerate(house_list):
-        # timestamp in millisecond
-        timestamp = int(round(time.time() * 1000))
-        # open a new file if reach eof
-        if not pretimes[house_id]:
-            # close opened file
-            if fileds[house_id]:
-                fileds[house_id].close()
-            filename = next(filenames[house_id], None)
-            # check if the end of the folder files, return to first file
+    filename_iters = [iter(app) for app in filenames]
+    for _ in range(int(filestart_offset)):
+        for hid, iter in enumerate(filename_iters):
+            filename = next(iter, None)
             if not filename:
-                filenames[house_id] = first_filenames[house_id]
-                filename = next(filenames[house_id])
-            fileds[house_id], csviters[house_id] = open_files(house_name, filename)
-            entries = next(csviters[house_id], None)
-            firsttimes[house_id] = pretimes[house_id] = float(entries[0])*1000
-            prevalues[house_id] = [float(x) if x != 'NULL' else 0 for x in entries[1:]]
-            offsets[house_id] = timestamp - float(entries[0])*1000
-        # check if the current passes the time inteval of the message
-        elif (pretimes[house_id]-firsttimes[house_id])/playbackspeed + offsets[house_id] + firsttimes[house_id] < timestamp:
-            for idx, power in enumerate(prevalues[house_id]):
-                value = {"timestamp": timestamp, "house_id": 2000+house_id, "appliance_id": idx, "appliance_name": labels[house_name][idx], "power": power}
-                print(value)
-                producer.produce(topic, key='key', value=json.dumps(value), callback=acked)
-            entries = next(csviters[house_id], None)
-            if entries:
-                # deal with ocassionally appeared column headers
-                if entries[0] == 'timestamp':
-                    entries = next(csviters[house_id])
-                pretimes[house_id] = float(entries[0])*1000
+                filename_iters[hid] = [iter(filenames[hid])]
+                next(filename_iters[hid])
+
+    # fileds: file descriptors for each house
+    # offsets: offset for the current time to the timestamp in the files
+    # firsttimes: first timestamp for the current file
+    # pretimes: previous timestamp for each house
+    # prevalues: previous power values for each house
+    # csviters: csv iterator for row reading
+    fileds = [None]*len(house_list)
+    pretimes = [None]*len(house_list)
+    firsttimes = [None]*len(house_list)
+    offsets = [None]*len(house_list)
+    csviters = [None]*len(house_list)
+    prevalues = [[] for _ in range(len(house_list))]
+    producer = kafka_init(brokers, topic)
+
+    while True:
+        for house_id, house_name in enumerate(house_list):
+            # timestamp in millisecond
+            timestamp = int(round(time.time() * 1000))
+            # open a new file if reach eof
+            if not pretimes[house_id]:
+                # close opened file
+                if fileds[house_id]:
+                    fileds[house_id].close()
+                filename = next(filename_iters[house_id], None)
+                # check if the end of the folder files, return to first file
+                if not filename:
+                    filename_iters[house_id] = iter(filenames[house_id])
+                    filename = next(filename_iters[house_id])
+                fileds[house_id], csviters[house_id] = open_files(house_name, filename)
+                entries = next(csviters[house_id], None)
+                firsttimes[house_id] = pretimes[house_id] = float(entries[0])*1000
                 prevalues[house_id] = [float(x) if is_number(x) else 0 for x in entries[1:]]
-            else:
-                pretimes[house_id] = prevalues[house_id] = None
+                offsets[house_id] = timestamp - float(entries[0])*1000
+            # check if the current passes the time inteval of the message
+            elif (pretimes[house_id]-firsttimes[house_id])/playbackspeed + offsets[house_id] + firsttimes[house_id] < timestamp:
+                for idx, power in enumerate(prevalues[house_id]):
+                    house_serial = '2_'+batch_num + '_' + str(house_id)
+                    value = {"timestamp": timestamp, "house_id": house_serial, "appliance_id": house_serial + '_' + str(idx), "appliance_name": labels[house_name][idx], "power": power * factor}
+                    print(value)
+                    producer.produce(topic, key='key', value=json.dumps(value), callback=acked)
+                entries = next(csviters[house_id], None)
+                if entries:
+                    # deal with ocassionally appeared column headers
+                    if entries[0] == 'timestamp':
+                        entries = next(csviters[house_id])
+                    pretimes[house_id] = float(entries[0])*1000
+                    prevalues[house_id] = [float(x) if is_number(x) else 0 for x in entries[1:]]
+                else:
+                    pretimes[house_id] = None
 
-    producer.flush()
-
-    i+=1
+        producer.flush()
--- ingestion/producer_REDD_batch.py
+++ ingestion/producer_REDD_batch.py
@@ -5,19 +5,33 @@ import json
 import csv
 import os
 import glob
+import configparser
+import sys
+import random
+import copy
 
-datapath = '../../data'
-servers = 'localhost:9092'
-topic = "powerraw"
-# how many time faster playback
-playbackspeed = 24
+def load_config(file):
+    """
+    Load config file
+    """
+    config = configparser.ConfigParser()
+    config.read(file)
 
+    datapath = config['Data']['redd_path']
+    brokers = config['KafkaBrokers']['address']
+    topic = config['KafkaBrokers']['topic_rawdata']
+    # how many time faster playback
+    playbackspeed = int(config['Data']['playback_speed'])
+    return datapath, brokers, topic, playbackspeed
 
-def house_reader_REDD(datapath):
+def house_reader_REDD(datapath, filestart_offset):
     '''
     Load the REDD files into file descriptors.
     @type datapath: STRING
     @param datapath: the path to the data folder
+
+    @type filestart_offset: int
+    @param filestart_offset: shift the starting point by ths number of entries
 
     @rtype fileds: list[list[file_descriptor]]
     @return: files descriptors for each appliances in each house
@@ -34,26 +48,34 @@ def house_reader_REDD(datapath):
     @rtype powers: list[list[float]]
     @return: first power values
     '''
-    path_REDD = datapath+'/REDD'
-    house_list=sorted(os.listdir(path_REDD))
+    house_list = sorted(os.listdir(datapath))
     fileds = [[] for _ in range(len(house_list))]
     fileiter = [[] for _ in range(len(house_list))]
-    labels = [[] for _ in range(len(house_list))]
+    labels =[[] for _ in range(len(house_list))]
     offsets = [[] for _ in range(len(house_list))]
     values = [[] for _ in range(len(house_list))]
+
     for hid, house in enumerate(house_list):
-        hpath = path_REDD + '/' + house
+        hpath = datapath + '/' + house
         for filename in glob.glob(hpath+"/channel_*.dat"):
             f = open(filename, newline='')
             fileds[hid].append(f)
             fileiter[hid].append(csv.reader(f, delimiter=' '))
             entries = next(fileiter[hid][-1])
+            # shift the staring entry
+            for _ in range(filestart_offset*86400//3):
+                entries = next(fileiter[hid][-1],None)
+                # read from the beginning if reaches the eof
+                if not entries:
+                    f.seek(0)
+                    entries = next(fileiter[hid][-1],None)
+
             # time offest in millisecond
             offsets[hid].append(int(round(time.time() * 1000)) - int(entries[0])*1000)
             # (timestamp, power)
             values[hid].append( (int(entries[0])*1000, float(entries[1])) )
         # read labels for the appliances
-        lpath = path_REDD + '/' + house + '/labels.dat'
+        lpath = datapath + '/' + house + '/labels.dat'
         labelf = open(lpath, newline='')
         lreader = csv.reader(labelf, delimiter=' ')
         for row in lreader:
@@ -99,37 +121,42 @@ def acked(err, msg):
         #print("Message produced: %s" % (str(msg)))
 
 
-i=0
-starttime=time.time()
-producer = kafka_init(servers, topic)
-fileds, fileiter, labels, offsets, values = house_reader_REDD(datapath)
 
-# Get the first timestamps for each appliances
-first_values = values
-#print(offsets)
-while True:
-    print(i)
-    try:
-        for house_id, appliances in enumerate(fileiter):
-            for app_id, row in enumerate(appliances):
-                # timestamp in millisecond
-                timestamp = int(round(time.time() * 1000))
-                # check if the end of file and if the current time elaspe is larger than the next data point
-                if values[house_id][app_id]:
-                    if (values[house_id][app_id][0]-first_values[house_id][app_id][0])/playbackspeed + offsets[house_id][app_id] + first_values[house_id][app_id][0] < timestamp:
-                        value = {"timestamp": timestamp, "house_id": 1000+house_id, "appliance_id": app_id, "appliance_name": labels[house_id][app_id], "power": values[house_id][app_id][1]}
-                        producer.produce(topic, key='key', value=json.dumps(value), callback=acked)
-                        # read next row
-                        entries = next(row, None)
-                        values[house_id][app_id] = (int(entries[0])*1000, float(entries[1])) if entries else None
-                else:
-                    fileds, fileiter, labels, offsets, values = house_reader_REDD(datapath)
-                    first_values = values
-                    # TO DO do this to individual files
-        producer.flush()
-    except KeyboardInterrupt:
-        close_files(fileds)
-        break
-    i += 1
 
-close_files(fileds)
+if __name__ == "__main__":
+    _, config, filestart_offset, batch_num = sys.argv
+    datapath, brokers, topic, playbackspeed = load_config(config)
+    # introduce randomness to the power output
+    factor = random.uniform(0.8, 1.2)
+    producer = kafka_init(brokers, topic)
+    fileds, fileiter, labels, offsets, values = house_reader_REDD(datapath, int(filestart_offset))
+    first_fileiter = copy.deepcopy(fileiter)
+    # Get the first timestamps for each appliances
+    first_values = copy.deepcopy(values)
+
+    while True:
+        try:
+            for house_id, appliances in enumerate(fileiter):
+                for app_id, row in enumerate(appliances):
+                    # timestamp in millisecond
+                    timestamp = int(round(time.time() * 1000))
+                    # check if the end of file and if the current time elaspe is larger than the next data point
+                    if values[house_id][app_id]:
+                        if (values[house_id][app_id][0]-first_values[house_id][app_id][0])/playbackspeed + offsets[house_id][app_id] + first_values[house_id][app_id][0] < timestamp:
+                            house_serial = '1_'+batch_num + '_' + str(house_id)
+                            value = {"timestamp": timestamp, "house_id": house_serial, "appliance_id": house_serial + '_' + str(app_id), "appliance_name": labels[house_id][app_id], "power": values[house_id][app_id][1] * factor}
+                            print(value)
+                            producer.produce(topic, key='key', value=json.dumps(value), callback=acked)
+                            # read next row
+                            entries = next(row, None)
+                            values[house_id][app_id] = (int(entries[0])*1000, float(entries[1])) if entries else None
+                    else:
+                        fileds[house_id][app_id].seek(0)
+                        entries = next(fileiter[house_id][app_id])
+                        offsets[house_id][app_id] = int(round(time.time() * 1000)) - int(entries[0])*1000
+                        first_values = (int(entries[0])*1000, float(entries[1]))
+            producer.flush()
+        except KeyboardInterrupt:
+            close_files(fileds)
+            break
+    close_files(fileds)
