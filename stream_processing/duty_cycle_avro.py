f5dab4abc58d2fd3c068f0ea5a1eb0efc9739b11
Kevin Lin <linkevinlin1@gmail.com>
2020-06-22 22:12:05 -0500

    stream script uses config parser and schema file

--- stream_processing/duty_cycle_avro.py
+++ stream_processing/duty_cycle_avro.py
@@ -1,76 +1,79 @@
 from pyspark.sql import SparkSession
 from pyspark.sql.types import *
-from pyspark.sql.functions import from_json,col
-from pyspark.sql.functions import window, to_json, when, count, struct
+from pyspark.sql.functions import window, to_json, when, count, struct, col
 from pyspark.sql.column import Column, _to_java_column
 from pyspark import SparkContext
+from typing import Tuple
+import configparser
 
-milli=1000
+milli = 1000
 
 
 def from_avro(col, jsonFormatSchema):
+    """
+    Convert Avro to JSON
+    """
     sc = SparkContext._active_spark_context
     avro = sc._jvm.org.apache.spark.sql.avro
     f = getattr(getattr(avro, "package$"), "MODULE$").from_avro
     return Column(f(_to_java_column(col), jsonFormatSchema))
 
 
-avro_schema = """
- {
-   "namespace": "powerraw",
-   "name": "valueall",
-   "type": "record",
-   "fields" : [
-     {
-       "name" : "house_id",
-       "type" : "string"
-     },
-     {
-       "name" : "appliance_name",
-       "type" : "string"
-     },
-     {
-       "name" : "appliance_id",
-       "type" : "string"
-     },
-     {
-       "name" : "timestamp",
-       "type" : "long"
-     },
-     {
-       "name" : "power",
-       "type" : "float"
-     }
-   ]
-}
-"""
+def load_config(filepath: str) -> Tuple[str, int, float, str, str, str, int]:
+    """
+    Load config file.
+
+    @param filepath: path to config.ini
+
+    @return window: time window in minutes
+
+    @return watermark: watermark window in seconds
+
+    @return powerthres: power threshold for active usage
+
+    @return brokers: kafka broker addresses
+
+    @return in_topic: kafka topic to read from
+
+    @return out_topic: kafka topic to write to
+
+    @return playbackspeed: how many times faster to playback data
+    """
+    config = configparser.ConfigParser()
+    config.read(filepath)
+
+    window = int(config['Spark']['window'])
+    watermark = config['Spark']['watermark']
+    powerthres = float(config['Spark']['powerthres'])
+    brokers = config['KafkaBrokers']['address']
+    in_topic = config['KafkaBrokers']['topic_rawdata']
+    out_topic = config['KafkaBrokers']['topic_stream']
+    # how many time faster playback
+    playbackspeed = int(config['Data']['playback_speed'])
+    return window, watermark, powerthres, brokers, in_topic, out_topic, playbackspeed
 
 
-kafka_servers = 'b-2.kafka6.2g20m5.c4.kafka.us-east-1.amazonaws.com:9092,b-1.kafka6.2g20m5.c4.kafka.us-east-1.amazonaws.com:9092,b-3.kafka6.2g20m5.c4.kafka.us-east-1.amazonaws.com:9092'
-in_topic = 'powerraw'
-out_topic = 'dutycycle'
-playbackspeed = 24
-window_len = 10
-powerthres = 5.0
-watermark = 2
-
+# read schema
+jsonFormatSchema = open("schema.avsc", "r").read()
+window_len, watermark, powerthres, kafka_servers, in_topic, out_topic, playbackspeed = load_config("config.ini")
 window_converted = window_len * 60 // playbackspeed
-
 
 spark = SparkSession.builder.appName("dutycycle").getOrCreate()
 
 df = spark \
   .readStream \
   .format("kafka") \
-  .option("kafka.bootstrap.servers", kafka_servers)\
+  .option("kafka.bootstrap.servers", kafka_servers) \
   .option("subscribe", in_topic) \
   .option("failOnDataLoss", "false") \
   .load()
 
-df=df.withColumn("value", from_avro("value", avro_schema))\
-  .select((col("value.timestamp")/milli).alias("time").cast(TimestampType()),col("value.house_id"),col("value.appliance_id"),col("value.appliance_name"),col("value.power")) #value.* for seperate columns
+df=df.withColumn("value", from_avro("value", jsonFormatSchema)) \
+  .select((col("value.timestamp")/milli).alias("time") \
+  .cast(TimestampType()),col("value.house_id"),col("value.appliance_id"),col("value.appliance_name"),col("value.power"))
 
-out=df.withWatermark("time", str(watermark) + " seconds").groupBy(window(col("time"), str(window_converted) + " seconds", str(window_converted//5) + " second"),"house_id","appliance_id") \
+out=df.withWatermark("time", watermark + " seconds") \
+  .groupBy(window(col("time"), str(window_converted) + " seconds", str(window_converted//5) + " second"),"house_id","appliance_id") \
   .agg(count("power").alias("c_all"),count(when(col("power") > powerthres, True)).alias("c_duty")) \
   .withColumn("duty_cycle", (col("c_duty")/col("c_all"))).withColumn("time_end", col("window.end")) \
   .drop("window", "c_all", "c_duty")
